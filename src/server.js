ab4452fd61713d44ef266f5f9b561c7e809d67d6
SousaPatricia25 <patricia.sousa2413@outlook.com>
2025-06-09 16:56:43 -0300

    express

---
+++ src/server.js
@@ -0,0 +1,42 @@
+
+import express from 'express';
+
+import { logger } from './middlewares/logger.js';
+
+const app = express();
+
+app.use(express.json ());
+
+// app.use(logger);
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log('Servidor rodando em http://localhost:3000.'));
+// // paramentros não nomeados.
+// app.get('/produtos/:categoria/:id', (req, res) => {
+// //   // navegador: http://localhost:3000/produtos/tenis/7
+//   const { categoria, id } = req.params;
+
+//   res.send(`Produto de categoria ${categoria} e id ${id}`);
+// //   // resposta: Produto de categoria tenis e id 7
+
+// });
+
+//parametros nomeados
+ app.get('/produtos', (req, res) => {
+
+  //Navegador: http://localhost:3000/produtos?pagina=1&limite=10
+  const {pagina, limite } = req.query;
+
+  res.send(`Página ${pagina} de ${limite}.`)
+  //resposta: Página 1 de 10
+ });
+
+ app.post('/produtos', logger, (req, res) =>{
+  const {nome, preco} = req.body;
+
+  const id= req.user_id;
+
+  res.status(201).json({id, nome, preco });
+
+ });
